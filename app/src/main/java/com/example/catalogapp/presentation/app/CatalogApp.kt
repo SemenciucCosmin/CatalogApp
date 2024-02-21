dbf8243cf002197450b870b7eff8c59b0676867a
Semenciuc <cosmin.semenciuc@garmin.com>
2024-02-21 13:54:35 +0200

    Add top app bar

--- app/src/main/java/com/example/catalogapp/presentation/app/CatalogApp.kt
+++ app/src/main/java/com/example/catalogapp/presentation/app/CatalogApp.kt
@@ -5,18 +5,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.catalogapp.navigation.BottomNavigationBar
 import com.example.catalogapp.navigation.LocalNavController
+import com.example.catalogapp.navigation.NavigationDestination
 import com.example.catalogapp.navigation.NavigationGraph
+import com.example.catalogapp.presentation.ui.compoenents.CatalogTopAppBar
 
 @Composable
 fun CatalogApp() {
     val navController = rememberNavController()
+    var currentRoute by remember { mutableStateOf(NavigationDestination.First.route) }
+    navController.addOnDestinationChangedListener { _, destination, _ ->
+        destination.route?.let { currentRoute = it }
+    }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        topBar = { CatalogTopAppBar(title = currentRoute) },
         bottomBar = { BottomNavigationBar(navController = navController) }
     ) { paddingValues ->
         CompositionLocalProvider(LocalNavController provides navController) {
---
+++ app/src/main/java/com/example/catalogapp/presentation/ui/compoenents/CatalogTopAppBar.kt
@@ -0,0 +1,26 @@
+package com.example.catalogapp.presentation.ui.compoenents
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import com.example.catalogapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CatalogTopAppBar(title: String) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = { }) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_top_app_bar),
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
